08d7f3dc9c3f6a4282c643916c5853696732a1c6
misi <mpekar55@gmail.com>
2020-07-23 15:19:34 +0200

    UserDao létrehozva

---
+++ service/src/main/java/hu/misi/feladat/service/dao/UserDao.java
@@ -0,0 +1,15 @@
+package hu.misi.feladat.service.dao;
+
+import hu.misi.feladat.core.model.Role;
+import hu.misi.feladat.core.model.User;
+
+import java.util.Collection;
+
+public interface UserDao {
+    Collection<User> getAllUsers();
+    Collection<User> getUsersByRole(Role role);
+    boolean modifyUser(User user);
+    User getUserById(int id);
+    User addUser(User user);
+    boolean deleteUserById(int id);
+}
